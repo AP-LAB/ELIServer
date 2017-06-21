d2546796083455857a3908a0fa242495cebcb045
Elize Herrewijnen <0891435@hr.nl>
2017-06-21 12:53:01 +0200

    Changed listening port

--- ELIServer/Messaging/MessageSocketManager.cs
+++ ELIServer/Messaging/MessageSocketManager.cs
@@ -26,7 +26,7 @@ namespace ELIServer
         private static MainWindow mainWindow; //!< A MainWindow instance where the state of the client lists can be updated.
         private TcpListener listener = null; //!< The TcpListener that is used to listen for new connections.
         private int port = 8005; //!< The port to listen to.
-        private string hostname = "127.0.0.1"; //!< The hostname to listen to.
+        private string hostname = "0.0.0.0"; //!< The hostname to listen to.
         private bool running = false; //!< A boolean that represents the state of the MessageSocketManager instance.
         private Thread thread; //!< The thread that is used for the StartListening() method.        
 
@@ -43,7 +43,7 @@ namespace ELIServer
             // Parse IP address
             IPAddress localAddr = IPAddress.Parse(hostname);
             // Create a new instance of TcpListener
-            listener = new TcpListener(IPAddress.Any, port);
+            listener = new TcpListener(localAddr, port);
             listener.Start();            
             // Create a new thread to handle the incoming messages.
             thread = new Thread(new ThreadStart(StartListening));
